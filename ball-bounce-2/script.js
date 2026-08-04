523e1906ff96f6695a614af3e8f95743be32c0e8
ikem10 <chibuikem.uka@dcmail.ca>
2026-08-03 21:27:17 -0400

    updates

--- ball-bounce-2/script.js
+++ ball-bounce-2/script.js
@@ -18,7 +18,14 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 // Ball class
-
+class Shape {
+  constructor(x, y, velX, velY) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+  }
+}
 class Ball {
   constructor(x, y, velX, velY, color, size) {
     this.x = x;
