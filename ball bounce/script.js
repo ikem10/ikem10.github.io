be135d51750e7eb3bc9ea7b4511b3b4bff6ba7fa
ikem10 <chibuikem.uka@dcmail.ca>
2026-08-03 21:03:14 -0400

    Update script.js

--- ball bounce/script.js
+++ ball bounce/script.js
@@ -17,6 +17,8 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+// Ball class
+
 class Ball {
   constructor(x, y, velX, velY, color, size) {
     this.x = x;
@@ -26,4 +28,96 @@ class Ball {
     this.color = color;
     this.size = size;
   }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  update() {
+
+    if (this.x + this.size >= width) {
+      this.velX = -this.velX;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.velX = -this.velX;
+    }
+
+    if (this.y + this.size >= height) {
+      this.velY = -this.velY;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY = -this.velY;
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+  collisionDetect() {
+
+    for (const ball of balls) {
+
+      if (this !== ball) {
+
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+
+          this.color = randomRGB();
+          ball.color = randomRGB();
+
+        }
+      }
+    }
+  }
 }
+
+
+// Create balls
+
+const balls = [];
+
+while (balls.length < 25) {
+
+  const size = random(10, 20);
+
+  const ball = new Ball(
+    random(size, width - size),
+    random(size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+
+  balls.push(ball);
+}
+
+
+// Animation loop
+
+function loop() {
+
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
