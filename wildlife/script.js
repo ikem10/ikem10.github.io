63f78afd539608b8a45b6223f23827bc44db4161
ikem10 <chibuikem.uka@dcmail.ca>
2026-08-12 22:26:10 -0400

    work

--- wildlife/script.js
+++ wildlife/script.js
@@ -6,15 +6,19 @@ const commentWrapper = document.querySelector('.comment-wrapper');
 commentWrapper.style.display = 'none';
 
 showHideBtn.onclick = function() {
-  let showHideText = showHideBtn.textContent;
-  if(showHideText === 'Show comments') {
+  let showHideText = showHideBtn.textContent.trim();
+
+  if (showHideText === 'Show comments') {
     showHideBtn.textContent = 'Hide comments';
     commentWrapper.style.display = 'block';
+    showHideBtn.setAttribute('aria-expanded', 'true');
   } else {
     showHideBtn.textContent = 'Show comments';
     commentWrapper.style.display = 'none';
+    showHideBtn.setAttribute('aria-expanded', 'false');
   }
 };
+
 
 // functionality for adding a new comment via the comments form
 
@@ -32,6 +36,7 @@ function submitComment() {
   const listItem = document.createElement('li');
   const namePara = document.createElement('p');
   const commentPara = document.createElement('p');
+
   const nameValue = nameField.value;
   const commentValue = commentField.value;
 
